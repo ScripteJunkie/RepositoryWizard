13bc7d767cc073b5932880d28a94d64e3260fa93
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2020-04-09 00:35:41 -0400

    Testing Commit

---
+++ sim.ps1
@@ -0,0 +1,20 @@
+param($jj)
+[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+Start-Sleep -Seconds 5.0
+[System.Windows.Forms.SendKeys]::SendWait("^+{a}")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait($jj)
+[System.Windows.Forms.SendKeys]::SendWait("~")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait("~")
+Start-Sleep -Seconds 2.0
+[System.Windows.Forms.SendKeys]::SendWait("^{n}")
+[System.Windows.Forms.SendKeys]::SendWait("Welcome to the README.")
+[System.Windows.Forms.SendKeys]::SendWait("^{s}")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait($jj)
+[System.Windows.Forms.SendKeys]::SendWait("~")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}")
+Start-Sleep -Seconds 3.0
+[System.Windows.Forms.SendKeys]::SendWait("READMEE.md")
+[System.Windows.Forms.SendKeys]::SendWait("~")
