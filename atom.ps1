f2f4f4b53c23c26d1ac6942d8174a2b8eb3aa9de
Ashton <30474771+ScripteJunkie@users.noreply.github.com>
2020-04-10 15:30:03 -0400

    Added Atom compatibility

--- atom.ps1
+++ atom.ps1
@@ -10,11 +10,16 @@ Start-Sleep -Seconds 5.0
 Start-Sleep -Seconds 2.0
 [System.Windows.Forms.SendKeys]::SendWait("^{n}")
 [System.Windows.Forms.SendKeys]::SendWait("Welcome to the README.")
+[System.Windows.Forms.SendKeys]::SendWait($jj)
 [System.Windows.Forms.SendKeys]::SendWait("^{s}")
-[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}")
+Start-Sleep -Seconds 2.0
+[System.Windows.Forms.SendKeys]::SendWait("READMEE.md")
+Start-Sleep -Seconds 2.0
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}")
+[System.Windows.Forms.SendKeys]::SendWait("~")
 [System.Windows.Forms.SendKeys]::SendWait($jj)
 [System.Windows.Forms.SendKeys]::SendWait("~")
-[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}")
 Start-Sleep -Seconds 3.0
-[System.Windows.Forms.SendKeys]::SendWait("READMEE.md")
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}")
 [System.Windows.Forms.SendKeys]::SendWait("~")
+[System.Windows.Forms.SendKeys]::SendWait("{F11}")
--- launch.bat
+++ launch.bat
@@ -1,6 +1,6 @@
 @echo off
-  ::git init %1
-  ::cd %1
+  git init %1
+  cd %1
   for /f "tokens=1 delims=:" %%L in ('findstr /n "<PATH>" source.xml') do (
    set begin_line=%%L
   )
@@ -8,9 +8,8 @@
   for /f "tokens=1 delims=:" %%L in ('findstr /n "</PATH>" source.xml') do (
    set /a end_line=%%L+1
   )
-
-  ::echo showing lines between %end_line% and %begin_line%
   break>"%temp%\empty"
+
   for /f "delims=" %%l in ('fc "%temp%\empty" "source.xml" /lb  %end_line% /t ^|more +4 ^| findstr /B /E /V "*****" ^| more +%begin_line%') do (
    set "lloc=%%l"
    goto :break_for
@@ -18,9 +17,11 @@
   :break_for
   ::echo %lloc%
   del /Q /F "%temp%\empty"
-  set jj = %1
+  ::set jj = %1
+  set jj = C:\Users\quads\Documents\GitHub\RepositoryWizard
   start %lloc%
-  Powershell.exe -executionpolicy remotesigned -File  %~dp0sim.ps1 "%jj%"
+  IF /I %2==Atom (
+  Powershell.exe -executionpolicy remotesigned -File  %~dp0atom.ps1 "%jj%")
   git add .
-  git commit -m "Testing Commit"
+  git commit -m "Initial Commit"
   git push
